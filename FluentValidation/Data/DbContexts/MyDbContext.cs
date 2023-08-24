1fad4ea000fa0260b204f06be5cc9606c85b9c75
f3xz1 <dgagnidzeh@gmail.com>
2023-08-24 23:10:11 +0400

    24.08.2023

---
+++ FluentValidation/Data/DbContexts/MyDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FluentValidation.Data.DbContexts
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(""); // constr
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<User>().Property(u => u.Name).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<User>().Property(u => u.Surname).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<User>().Property(u => u.Username).IsRequired().HasMaxLength(20);
+            modelBuilder.Entity<User>().Property(u => u.Password).IsRequired().HasMaxLength(100); 
+            modelBuilder.Entity<User>().Property(u => u.Email).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<User>().Property(u => u.Age).IsRequired();
+        }
+    }
+
+
+}
---
+++ FluentValidation/Data/Models/User.cs
@@ -0,0 +1,13 @@
+﻿public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+}
+
+// Db должен быть удаленным для того чтобы я быстро подклчился
+// Bcrypt, Cookies, UserClaims, What is JWT ? 
